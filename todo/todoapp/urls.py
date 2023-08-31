a797f24bb37fc07ff3ee8d64fb71dc85273876ff
vishnukk302 <vishnukk302@gmail.com>
2023-08-31 10:12:12 +0530

    added

---
+++ todo/todoapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.todo, name='todo'),
+    path('delete/<int:taskid>/', views.delete, name='delete'),
+    path('update/<int:id>/', views.update, name='update'),
+    path('cbvhome/', views.Tasklistview.as_view(), name='cbvhome'),
+    path('cbvdetail/<int:pk>/', views.TaskDetailview.as_view(), name='cbvdetail'),
+    path('cbvupdate/<int:pk>/', views.TaskUpdate.as_view(), name='cbvupdate'),
+    path('cbvdelete/<int:pk>/', views.TaskDeleteview.as_view(), name='cbvdelete'),
+]
